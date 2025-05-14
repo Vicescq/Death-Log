8ba4d502f5dc4785428e3b55ff06a28c5d975085
Hanss <hanss.rivera@gmail.com>
2025-05-14 15:11:50 -0600

    checkpoint

--- client/src/pages/GameProfiles.tsx
+++ client/src/pages/GameProfiles.tsx
@@ -2,6 +2,9 @@ import { useLocation } from "react-router";
 import Card from "../components/Card";
 import Game from "../classes/Game";
 import { reconstructCollection } from "../utils";
+import { useState } from "react";
+import Profile from "../classes/Profile";
+import UtilityCard from "../components/UtilityCard";
 
 export default function GameProfiles() {
     const location = useLocation();
@@ -9,11 +12,37 @@ export default function GameProfiles() {
     if (location.state) {
         objContext = reconstructCollection(location.state, Game)
     }
-    console.log(objContext)
+
+    const [profiles, setProfiles] = useState<Profile[]>([]);
+    const [addProfileText, setaddProfileText] = useState("");
+    const [delProfileText, setdelProfileText] = useState("");
+
+    function handleAddProfileBtn() {
+        if (addProfileText != "") {
+            const newProfile = new Profile(addProfileText, []);
+            setProfiles((prevProfiles) => [...prevProfiles, newProfile]);
+        }
+    }
+
+    function handelDelProfileBtn() {
+        if (delProfileText != "") {
+            setProfiles((prevProfiles) => prevProfiles.filter(
+                (_, index) => index !== Number(delProfileText)
+            ));
+        }
+    }
+
+
     if (location.state) {
         return (
             <div className="flex items-center justify-center gap-2 m-8">
-                <Card objContext={objContext} />
+                <UtilityCard addOrDelStr="Add game" handleBtn={handleAddProfileBtn} handleTextChange={(event) => setaddProfileText(event.target.value)} />
+
+                {
+                    profiles.map(
+                        (profile, index) => (<Card key={index} objContext={profile} />)
+                    )
+                }
             </div>
         )
     }
---
+++ client/src/pages/ProfileSubjects.tsx
@@ -0,0 +1,12 @@
+import type Collection from "../classes/Collection";
+import Card from "../components/Card";
+
+export default function ProfileSubjects() {
+    
+    
+    return (
+        <div className="flex items-center justify-center gap-2 m-8">
+            <Card objContext={objContext} />
+        </div>
+    )
+}
