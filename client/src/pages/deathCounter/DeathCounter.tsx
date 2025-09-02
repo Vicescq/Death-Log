088022909dfe1e757d15a52f9884a071f1e90a46
Hanss <hanss.rivera@gmail.com>
2025-09-02 14:45:29 -0600

    death counter start

---
+++ client/src/pages/deathCounter/DeathCounter.tsx
@@ -0,0 +1,14 @@
+import { useTreeStore } from "../../stores/useTreeStore"
+
+export default function DeathCounter(){
+    const tree = useTreeStore((state) => state.tree);
+
+    return 
+    <div>
+
+
+
+
+
+    </div>
+}
