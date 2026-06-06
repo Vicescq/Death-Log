eb2a3bb831b1da4cd64b91695a380a9c9cc539ff
Hanss Rivera <hanss.rivera@gmail.com>
2026-06-06 01:45:07 -0600

    renamed playwright tests folder to e2e

--- client/playwright.config.ts
+++ client/playwright.config.ts
@@ -13,7 +13,7 @@ import { CONSTANTS } from './shared/constants';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests',
+  testDir: './e2e',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
